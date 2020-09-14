eca0b40633027d00d591f0c774cf56790d82c1c2
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-14 18:49:48 -0400

    GameMaster explains rules

--- lib/game_master.rb
+++ lib/game_master.rb
@@ -11,5 +11,13 @@ class GameMaster
     end
   end
 
-  def explain_rules; end
+  def explain_rules
+    puts "\nLet me explain the rules.\n\nThe board has 3 rows each with 3 columns.\n\n row1: col1, col2, col3\n row2: col1, col2, col3\n row3: col1, col2, col3\n\nFor this example, I am \"X\"\nI answer \"row2 col2\"\n\n [\" \",\" \",\" \"]\n [\" \",\"X\",\" \"]\n [\" \",\" \",\" \"]\n\nEach your turn you shall place your symbol.\nFirst one to place 3 consecutive symbols wins.\nThey can be vertically, horizontally or diagonally aligned."
+    puts 'Do you understand? (Yes or No)'
+    understand_rules = gets.chomp
+    if understand_rules == 'Yes'
+      Board.new
+    else explain_rules
+    end
+  end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,4 @@
 # game_master.rb
 require_relative 'game_master'
 
-new_game = GameMaster.new
+GameMaster.new
