67eb883ac304d7f4d339fd6274a136b0cc7eb007
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-15 18:36:57 -0400

    logic starting to make sense

--- lib/game.rb
+++ lib/game.rb
@@ -2,16 +2,77 @@
 
 # Create board and check winner
 class Game
-  def initialize
-    row1 = [' ', ' ', ' ']
-    row2 = [' ', ' ', ' ']
-    row3 = [' ', ' ', ' ']
-    create_board(row1, row2, row3)
+  def initialize(row1, row2, row3, player1_symbol, player2_symbol)
+    @row1 = row1
+    @row2 = row2
+    @row3 = row3
+    @player1_symbol = player1_symbol
+    @player2_symbol = player2_symbol
+    @player1 = true
+    create_board
   end
 
-  def create_board(row1, row2, row3)
-    p row1
-    p row2
-    p row3
+  def create_board
+    @row1.push(' ', ' ', ' ')
+    @row2.push(' ', ' ', ' ')
+    @row3.push(' ', ' ', ' ')
+    display_board
+  end
+
+  def display_board
+    p @row1
+    p @row2
+    p @row3
+    play_turn
+  end
+
+  def play_turn
+    if @player1 == true
+      puts 'Player 1, where do you want to play?'
+      player_move = gets.chomp
+      @player1 = false
+      symbol_placement(player_move, @player1_symbol)
+    else
+      puts 'Player 2, where do you want to play?'
+      player_move = gets.chomp
+      @player1 = true
+      symbol_placement(player_move, @player2_symbol)
+    end
+  end
+
+  def symbol_placement(player_move, player_symbol)
+    case player_move
+    when 'row1 col1'
+      check_valid
+      @row1[0] = player_symbol
+    when 'row1 col2'
+      check_valid
+      @row1[1] = player_symbol
+    when 'row1 col3'
+      check_valid
+      @row1[2] = player_symbol
+    when 'row2 col1'
+      check_valid
+      @row2[0] = player_symbol
+    when 'row2 col2'
+      check_valid
+      @row2[1] = player_symbol
+    when 'row2 col3'
+      check_valid
+      @row2[2] = player_symbol
+    when 'row3 col1'
+      check_valid
+      @row3[0] = player_symbol
+    when 'row3 col2'
+      check_valid
+      @row3[1] = player_symbol
+    when 'row3 col3'
+      check_valid
+      @row3[2] = player_symbol
+    else
+      puts 'Invalid play, try again!'
+      @player1 = !(@player1 == true)
+      play_turn
+    end
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -4,19 +4,19 @@
 class Player
   def initialize
     puts "Player 1, do you want \"X\" or \"O\"?\nEnter \"X\" or \"O\"."
-    @player_1_symbol = gets.chomp
-    player_symbols(@player_1_symbol)
+    player1_symbol = gets.chomp
+    player_symbols(player1_symbol)
   end
 
-  def player_symbols(player_1_symbol)
-    if player_1_symbol == 'X'
-      @player_2_symbol = 'O'
+  def player_symbols(player1_symbol)
+    if player1_symbol == 'X'
+      player2_symbol = 'O'
       puts "Player 1 is \"X\" and Player 2 is \"O\".\n "
-      Game.new
+      Game.new([], [], [], player1_symbol, player2_symbol)
     elsif player_1_symbol == 'O'
-      @player_2_symbol = 'X'
+      player2_symbol = 'X'
       puts "Player 1 is \"O\" and player 2 is \"X\".\n "
-      Game.new
+      Game.new([], [], [], player1_symbol, player2_symbol)
     else initialize
     end
   end
